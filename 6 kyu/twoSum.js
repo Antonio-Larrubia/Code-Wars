164a221668dbd9f9417507c5ae4a2ff7a8280a16
antonio <eal311088@gmail.com>
2021-04-12 18:28:42 -0300

    Adds better way to do twoSum

--- 6 kyu/twoSum.js
+++ 6 kyu/twoSum.js
@@ -27,3 +27,13 @@ function twoSum(numbers, target) {
 console.log(twoSum([1,2,3], 4)) //[0,2]
 console.log(twoSum([1234,5678,9012], 14690)) // [1,2]
 console.log(twoSum([2,2,3], 4)) //[0,1]
+
+//Better way
+
+function twoSum(numbers, target) {
+  for (var i = 0; i < numbers.length-1; i++) {
+      for (var j = i+1; j < numbers.length; j++) {
+          if (numbers[i] + numbers[j] === target) return [i, j];
+      }
+  }
+}
