20d2fbbd0edbb92f5af496b2e681d57496ecfd2f
antonio <eal311088@gmail.com>
2021-04-27 00:14:25 -0300

    Adds better version of whoisthis.js

--- string_practice/who_likes_it.js
+++ string_practice/who_likes_it.js
@@ -38,6 +38,19 @@ function likes(names) {
     return str;
 }
 
+// BETTER VERSION
+
+function likes(names) {
+    names = names || [];
+    switch(names.length){
+      case 0: return 'no one likes this'; break;
+      case 1: return names[0] + ' likes this'; break;
+      case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+      case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+      default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    }
+  }
+
 //console.log(likes ([])) //-- must be "no one likes this"
 //console.log(likes (["Peter"])) //-- must be "Peter likes this"
 console.log(likes (["Jacob", "Alex"])) //-- must be "Jacob and Alex like this"
